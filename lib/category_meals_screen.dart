08f2f3239dacb90b68c181c2b72ff0e04f2bb80a
Enzo CONTY <contact@enzoconty.dev>
2019-12-24 15:12:02 +0100

    Navigator

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("2nd screen here !"),
+      ),
+      body: Center(
+        child: Text("Coucou !"),
+      ),
+    );
+  }
+}

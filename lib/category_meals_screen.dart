bf02f3f68991132624c4de87f8703b03affae7bb
Enzo CONTY <contact@enzoconty.dev>
2020-01-02 17:04:43 +0100

    Meal Model // styling of category_meal_screen

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-
   /* final String categoryId;
   final String categoryTitle;
 
@@ -9,15 +9,22 @@ class CategoryMealsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
-        title: Text(categoryTitle ),
+        title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text(categoryId),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }

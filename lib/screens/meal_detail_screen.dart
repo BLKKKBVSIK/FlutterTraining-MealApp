d2397dd91795189d774b3e2d50f09e41bc57eac7
Enzo CONTY <contact@enzoconty.dev>
2020-01-09 16:04:29 +0100

    Meal Detail Screen Styling

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -4,6 +4,42 @@ import 'package:meal_app/dummy_data.dart';
 class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
 
+  Widget buildSectionTitle(BuildContext context, String text) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        text,
+        style: Theme.of(context).textTheme.title,
+      ),
+    );
+  }
+
+  Widget buildContainer(Widget child) {
+    return Container(
+      height: 100,
+      width: 300,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10)),
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      child: child,
+    );
+  }
+
+  /*
+
+ListView.builder(
+                itemCount: selectedMeal.ingredients.length,
+                itemBuilder: (ctx, index) => Padding(
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    child: Text(selectedMeal.ingredients[index])),
+              )
+
+
+  */
+
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
@@ -13,43 +49,56 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(selectedMeal.title),
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: (MediaQuery.of(context).size.height - AppBar().preferredSize.height - MediaQuery.of(context).padding.top) * 0.3,
-            width: double.infinity,
-            child: Image.network(
-              selectedMeal.imageUrl,
-              fit: BoxFit.cover,
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10),
-            child: Text(
-              'Ingredient',
-              style: Theme.of(context).textTheme.title,
-            ),
-          ),
-          Container(
-            height: 100,
-            width: 300,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(10)),
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.all(10),
-            child: ListView.builder(
-              itemCount: selectedMeal.ingredients.length,
-              itemBuilder: (ctx, index) => Card(
-                color: Theme.of(context).accentColor,
-                child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                    child: Text(selectedMeal.ingredients[index])),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  new BoxShadow(
+                    color: Colors.grey.shade900,
+                    blurRadius: 15.0,
+                  )
+                ],
+              ),
+              height: (MediaQuery.of(context).size.height -
+                      AppBar().preferredSize.height -
+                      MediaQuery.of(context).padding.top) *
+                  0.3,
+              width: double.infinity,
+              child: Image.network(
+                selectedMeal.imageUrl,
+                fit: BoxFit.cover,
               ),
             ),
-          )
-        ],
+            buildSectionTitle(context, 'Ingredients'),
+            Container(
+              child: Column(
+                children: <Widget>[
+                  for (var i in selectedMeal.ingredients) Text(i),
+                ],
+              ),
+            ),
+            SizedBox(
+                height: (MediaQuery.of(context).size.height -
+                        AppBar().preferredSize.height -
+                        MediaQuery.of(context).padding.top) *
+                    0.05),
+            buildSectionTitle(context, 'Steps'),
+            Column(
+              children: <Widget>[
+                for (var i = 0; i < selectedMeal.steps.length; i++)
+                  ListTile(
+                    leading: CircleAvatar(
+                      child: Text("#${i + 1}"),
+                    ),
+                    title: Text(selectedMeal.steps[i]),
+                  ),
+                Divider(),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
